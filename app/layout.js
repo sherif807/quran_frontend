493b20b2d7e11b9bb9921ed68f13966995aad492
sherif807 <sbadawy807@gmail.com>
2026-02-26 00:14:29 -0500

    clarity

--- app/layout.js
+++ app/layout.js
@@ -99,7 +99,6 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  const gaId = process.env.NEXT_PUBLIC_GA_ID || "G-WGKCH9GJTW";
   return (
     <html
       lang="en"
@@ -149,22 +148,15 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className="bg-light">
-        {gaId && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-              strategy="afterInteractive"
-            />
-            <Script id="ga-init" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${gaId}');
-              `}
-            </Script>
-          </>
-        )}
+        <Script id="clarity-init" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vn98f32q8a");
+          `}
+        </Script>
         <div className="container py-3">{children}</div>
       </body>
     </html>
